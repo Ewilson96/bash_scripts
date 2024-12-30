6bb4370f158c73355b7867e9b9ddbd3cdaa88498
Ewilson96 <ennil.wilson@outlook.com>
2024-12-30 13:32:58 -0500

    Update zmc_check_agents.sh

--- zmc_check_agents.sh
+++ zmc_check_agents.sh
@@ -2,7 +2,7 @@
 
 output_file="/users/ewilson/zmc_files/logs/connected_endpoints.csv"
 : > "$output_file"
-curl --insecure -s -c logs/cookies.txt -X POST https://localhost:7443/api/v2/users/logIn -d 'username=superuser&password=Image4Jup!' 1> /dev/null
+curl --insecure -s -c logs/cookies.txt -X POST https://localhost:7443/api/v2/users/logIn -d 'username=xxxx&password=xxxx' 1> /dev/null
 sleep 0.5
 echo -e "Check performed on: $(date)\nStatus,Hostname,ipAddress,lastCheckin,groups,OS\n" | tee -a "$output_file" > /dev/null
 curl --insecure -s -b logs/cookies.txt https://localhost:7443/api/v2/endpoints/summary \
